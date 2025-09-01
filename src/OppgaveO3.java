ba072e1cbbb9576ccc498725b216a661817aac23
Koding667278 <667278@stud.hvl.no>
2025-09-01 12:54:54 +0200

    Add files via upload

--- src/OppgaveO3.java
+++ src/OppgaveO3.java
@@ -1,30 +1,34 @@
-
-import static javax.swing.JOptionPane.*;
+import java.util.Scanner;
 
 public class OppgaveO3 {
 
 	public static void main(String[] args) {
-		
+    Scanner sc = new Scanner(System.in);
 		boolean validInput = false;
-		while (!validInput) {
-			
-			String Fakultetantall= showInputDialog("Skriv inn eit heltall n>0");
-			
+		
+    while (!validInput) {
+			System.out.print("Skriv inn eit heltall n>0: ");
+      String input = sc.nextLine();
+
+      
+
 		try {
-			int n = Integer.parseInt(Fakultetantall);
+			int n = Integer.parseInt(input.trim());
 			if (n>0) {
 				int Fakultet = 1;
 				for (int i =1;i <= n; i++ ) {
 					Fakultet *=i;
 				}
-				showMessageDialog(null, "Verdien av " + n + "! er " + Fakultet);
+				System.out.println("Verdien av " + n + "! er " + Fakultet);
 				validInput = true;
 			}
 			else
-				showMessageDialog(null,"Du har skrevet en ugyldig verdi for n");
+				System.out.println("Du har skrevet en ugyldig verdi for n");
 	} catch (NumberFormatException e) {
-				showMessageDialog(null, "Du har skrevet inn eit ugyldig heltall over 0");
+				System.out.println("Du har skrevet inn eit ugyldig heltall over 0");
 		}
 	}
+
+  sc.close();
  }
 }
